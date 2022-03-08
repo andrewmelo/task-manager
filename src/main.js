061747e54e04a1336ff7b5fe5f91483c92584132
Andrew Melo <andrewmelo42@gmail.com>
2022-03-08 18:55:31 -0300

    Small updates

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.setHeader('Content-Type', 'text/html');
+  //res.end('Hello World!');
+  fs.createReadStream('src/index.html').pipe(res)
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+console.log(process.env.TESTE)
